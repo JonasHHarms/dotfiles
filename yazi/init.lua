8ee2ffbc6635b1c1ac9bd0e1ca095a31f9ecf018
JonasHHarms <jonas@harms360.com>
2026-02-09 22:55:21 +0100

    yazi fix

--- yazi/init.lua
+++ yazi/init.lua
@@ -34,3 +34,18 @@ Status:children_add(function()
 		" ",
 	})
 end, 500, Status.RIGHT)
+
+
+function Linemode:size_and_mtime()
+	local time = math.floor(self._file.cha.mtime or 0)
+	if time == 0 then
+		time = ""
+	elseif os.date("%Y", time) == os.date("%Y") then
+		time = os.date("%d.%m.%y %H:%M", time)
+	else
+		time = os.date("%d.%m.%y %H:%M", time)
+	end
+
+	local size = self._file:size()
+	return string.format("%s %s", size and ya.readable_size(size) or " ", time)
+end
