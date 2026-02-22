381f821450b2a6a2b78adf38ab78a161b49565e7
JonasHarms <jonas@harms360.com>
2026-02-22 17:19:30 +0100

    fixes for tower

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,5 +1,18 @@
 -- Plugins
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
@@ -86,13 +99,9 @@ config = function()
 end,
 },
     { "nvim-treesitter/nvim-treesitter", version = false,
-    build = false,
     config = function()
-        require 'nvim-treesitter.install'.prefer_git = false
+        require 'nvim-treesitter.install'.prefer_git = true
         require 'nvim-treesitter.install'.compilers = { "cl", "clang", "gcc", "zig" }
-        require("nvim-treesitter.configs").setup({
-            auto_install = false,
-        })
     end
 },
 {"nvim-telescope/telescope.nvim", cmd = "Telescope", version = false,
