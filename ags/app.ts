5954d00f82754245055ce7dd66f3d7c485828473
JonasHarms <jonas@harms360.com>
2026-03-14 17:19:04 +0100

    ags systray

--- ags/app.ts
+++ ags/app.ts
@@ -1,10 +1,16 @@
 import app from "ags/gtk4/app"
 import style from "./style.scss"
-import Bar from "./widget/Bar"
+import GLib from "gi://GLib"
+import Systray from "./widget/systray"
+import Gtk from "gi://Gtk?version=4.0"
+let systray: Gtk.Window
 
 app.start({
   css: style,
+  gtkTheme: "Adwaita-dark",
   main() {
-    app.get_monitors().map(Bar)
+      systray = Systray() as Gtk.Window
+      app.add_window(systray)
+      systray.present()
   },
 })
--- ags/widget/systray.tsx
+++ ags/widget/systray.tsx
@@ -1,13 +1,52 @@
-const systemtray = await Service.import('systemtray')
+import app from "ags/gtk4/app"
+import GLib from "gi://GLib"
+import AstalTray from "gi://AstalTray"
+import { Astal, Gtk, Gdk } from "ags/gtk4"
+import { For, With, createBinding, onCleanup, createState } from "ags"
 
-/** @param {import('types/service/systemtray').TrayItem} item */
-const SysTrayItem = item => Widget.Button({
-    child: Widget.Icon().bind('icon', item, 'icon'),
-    tooltipMarkup: item.bind('tooltip_markup'),
-    onPrimaryClick: (_, event) => item.activate(event),
-    onSecondaryClick: (_, event) => item.openMenu(event),
-});
+export default function Systray() {
+    onCleanup(() => {
+      win.destroy()
+    })
+    const win = new Gtk.Window({
+      title: "Systray",
+      defaultWidth: 300,
+      defaultHeight: 50,
+      decorated: true,
+    })
 
-const sysTray = Widget.Box({
-    children: systemtray.bind('items').as(i => i.map(SysTrayItem))
-})
+    function Tray() {
+      const tray = AstalTray.get_default()
+      const items = createBinding(tray, "items")
+      const init = (btn: Gtk.MenuButton, item: AstalTray.TrayItem) => {
+        btn.menuModel = item.menuModel
+        btn.insert_action_group("dbusmenu", item.actionGroup)
+        item.connect("notify::action-group", () => {
+          btn.insert_action_group("dbusmenu", item.actionGroup)
+        })
+      }
+      return (
+        <box>
+          <For each={items}>
+            {(item) => (
+              <menubutton $={(self) => init(self, item)}>
+                <image gicon={createBinding(item, "gicon")} />
+              </menubutton>
+            )}
+          </For>
+        </box>
+        )
+      }
+  const content = (
+    <box orientation={Gtk.Orientation.HORIZONTAL}>
+      <label label="Systemtray" />
+      <Tray />
+    </box>
+  )
+
+  win.set_child(content as Gtk.Widget)
+  app.add_window(win)
+  return win
+
+}
+
