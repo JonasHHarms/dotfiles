c2c6693db9b17dc3fa1c6c22fe4ae7857f1b8e92
JonasHarms <jonas@harms360.com>
2026-03-14 14:57:48 +0100

    new alt-tab

--- hypr/scripts/alttab/disable.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env zsh
-hyprctl -q keyword animations:enabled true
-
-hyprctl -q --batch "keyword unbind ALT, TAB ; keyword unbind ALT SHIFT, TAB ; keyword bind ALT, TAB, exec, $HOME/.config/hypr/scripts/alttab/enable.sh 'down' ; keyword bind ALT SHIFT, TAB, exec, $HOME/.config/hypr/scripts/alttab/enable.sh 'up'"
